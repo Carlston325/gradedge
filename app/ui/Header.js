501e7cbf09a5e8a6ac009034ae8ffeb1d8d9246d
Carlston325 <130921730+Carlston325@users.noreply.github.com>
2025-08-05 11:37:01 +0100

    header styled

--- app/ui/Header.js
+++ app/ui/Header.js
@@ -1,28 +1,90 @@
+"use client";
+import ShieldIcon from "@mui/icons-material/Shield";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Header() {
+  const [activeTab, setActiveTab] = useState("home");
+
+  function handleClickedTab(tab) {
+    setActiveTab(tab);
+  }
+
   return (
     <header>
-      <h2>GRADEDGE</h2>
+      <h2>
+        <span>
+          <ShieldIcon />
+        </span>
+        gradedge
+      </h2>
       <nav>
         <ul>
           <li>
-            <Link href="/">Home</Link>
+            <Link
+              href="/"
+              className={activeTab === "home" ? "active-tab" : null}
+              onClick={() => {
+                handleClickedTab("home");
+              }}
+            >
+              Home
+            </Link>
           </li>
           <li>
-            <Link href="/cv">Cv</Link>
+            <Link
+              href="/cv"
+              className={activeTab === "cv" ? "active-tab" : null}
+              onClick={() => {
+                handleClickedTab("cv");
+              }}
+            >
+              Cv
+            </Link>
           </li>
           <li>
-            <Link href="/cover-letter">Cover Letter</Link>
+            <Link
+              href="/cover-letter"
+              className={activeTab === "cover-letter" ? "active-tab" : null}
+              onClick={() => {
+                handleClickedTab("cover-letter");
+              }}
+            >
+              Cover Letter
+            </Link>
           </li>
           <li>
-            <Link href="/ats-checker">ATS Checker</Link>
+            <Link
+              href="/ats-checker"
+              className={activeTab === "ats-checker" ? "active-tab" : null}
+              onClick={() => {
+                handleClickedTab("ats-checker");
+              }}
+            >
+              ATS Checker
+            </Link>
           </li>
           <li>
-            <Link href="/features">Features</Link>
+            <Link
+              href="/features"
+              className={activeTab === "features" ? "active-tab" : null}
+              onClick={() => {
+                handleClickedTab("features");
+              }}
+            >
+              Features
+            </Link>
           </li>
           <li>
-            <Link href="/profile">Profile</Link>
+            <Link
+              href="/profile"
+              className={activeTab === "profile" ? "active-tab" : null}
+              onClick={() => {
+                handleClickedTab("profile");
+              }}
+            >
+              Profile
+            </Link>
           </li>
         </ul>
       </nav>
