25a58cfa5f7a43472bded80c8286f9a3df4a0cd0
Carlston325 <130921730+Carlston325@users.noreply.github.com>
2025-11-11 22:40:30 +0000

    hero updated, colours updated

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -5,10 +5,7 @@ import PauseIcon from "@mui/icons-material/Pause";
 import VolumeUpIcon from "@mui/icons-material/VolumeUp";
 import VolumeOffIcon from "@mui/icons-material/VolumeOff";
 
-export default function Hero(props) {
-  const [heroImgSrc, setHeroImgSrc] = useState("/cv-image.jpg");
-  const [heroImgeAlt, setHeroImgAlt] = useState("A sample CV image");
-
+export default function Hero() {
   useEffect(() => {
     const objOne = document.querySelector(".obj-1");
     const objTwo = document.querySelector(".obj-2");
@@ -16,9 +13,9 @@ export default function Hero(props) {
 
     setInterval(() => {
       function checkClass() {
-        if (objOne.className.includes("bold")) {
+        if (objOne.className.includes("font-bold")) {
           return 2;
-        } else if (objTwo.className.includes("bold")) {
+        } else if (objTwo.className.includes("font-bold")) {
           return 3;
         } else {
           return 1;
@@ -27,30 +24,33 @@ export default function Hero(props) {
 
       switch (checkClass()) {
         case 1:
-          objOne.className = "obj-1 bold";
+          objOne.className = "obj-1 font-bold";
           objTwo.className = "obj-2";
           objThree.forEach((obj) => (obj.className = "obj-3"));
 
-          setHeroImgSrc("/cv-image.jpg");
-          setHeroImgAlt("A sample CV image");
+          objOne.style.color = "var(--gold)";
+          objTwo.style.color = "var(--text-light)";
+          objThree.forEach((obj) => (obj.style.color = "var(--text-light)"));
           break;
 
         case 2:
           objOne.className = "obj-1";
-          objTwo.className = "obj-2 bold";
+          objTwo.className = "obj-2 font-bold";
           objThree.forEach((obj) => (obj.className = "obj-3"));
 
-          setHeroImgSrc("/cover-letter-image.png");
-          setHeroImgAlt("A sample Cover Letter image");
+          objOne.style.color = "var(--text-light)";
+          objTwo.style.color = "var(--gold)";
+          objThree.forEach((obj) => (obj.style.color = "var(--text-light)"));
           break;
 
         case 3:
           objOne.className = "obj-1";
           objTwo.className = "obj-2";
-          objThree.forEach((obj) => (obj.className = "obj-3 bold"));
+          objThree.forEach((obj) => (obj.className = "obj-3 font-bold"));
 
-          setHeroImgSrc("/ats-checker-image.png");
-          setHeroImgAlt("A sample ATS checker image");
+          objOne.style.color = "var(--text-light)";
+          objTwo.style.color = "var(--text-light)";
+          objThree.forEach((obj) => (obj.style.color = "var(--gold)"));
           break;
 
         default:
@@ -77,6 +77,23 @@ export default function Hero(props) {
     }
   }
 
+  useEffect(() => {
+    if (videoRef.current) {
+      if (videoRef.current.muted === false) {
+        videoRef.current.muted = "true";
+
+        handleVideoToggle(false);
+        handleAudio(true);
+
+        // setTimeout(() => {
+        //   handleVideoToggle(true);
+        //   handleAudio(false);
+        // }, [1000]);
+      }
+      console.log(videoRef.current.muted);
+    }
+  }, []);
+
   return (
     <div className="hero">
       <div className="hero-title">
@@ -84,7 +101,10 @@ export default function Hero(props) {
           Hello, I am <span>JobEdge</span>
         </h2>
         <h3>But what matters is.</h3>
-        <h1 className="title">Who Are You ______</h1>
+        <h1 className="title">Who Are You {/* <span>___________</span> */}</h1>
+        <label htmlFor="user-name-input">
+          <input id="user-name-input" type="text" placeholder="_______"></input>
+        </label>
       </div>
       <div className="hero-video">
         <video
@@ -101,31 +121,36 @@ export default function Hero(props) {
         {volumeIsOff ? (
           <VolumeOffIcon
             onClick={() => handleAudio(false)}
-            className="text-white/20"
+            className="text-white/20 cursor-pointer"
           />
         ) : (
           <VolumeUpIcon
             onClick={() => handleAudio(true)}
-            className="text-white/50"
+            className="text-white/50 cursor-pointer"
           />
         )}
         {playVideo ? (
           <PauseIcon
             onClick={() => handleVideoToggle(false)}
-            className="text-white/50"
+            className="text-white/50 cursor-pointer"
           />
         ) : (
           <PlayArrowIcon
-            onClick={() => handleVideoToggle(true)}
-            className="text-white/50"
+            onClick={() => {
+              handleVideoToggle(true);
+              handleAudio(false);
+            }}
+            className="text-white/50 cursor-pointer"
           />
         )}
       </div>
       <div className="hero-description">
         <p>
           I am a useful tool to help you create a professional{" "}
-          <span className="obj-1 bold">CV</span>,{" "}
-          <span className="obj-2"> Cover Letter</span>, and ATS-friendly
+          <span className="obj-1 font-bold" style={{ color: "var(--gold)" }}>
+            CV
+          </span>
+          , <span className="obj-2"> Cover Letter</span>, and ATS-friendly
           documents. I also provide an{" "}
           <span className="obj-3">ATS Checker</span> to ensure your documents
           pass through <span className="obj-3">Applicant Tracking Systems</span>
